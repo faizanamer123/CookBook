a0802c510f3f3f9e51425926f1420fad26b21389
faizanamer123 <faizan.amer.390@gmail.com>
2025-06-09 03:33:13 +0500

    feat: Add configuration file with API endpoints

---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'http://localhost:5000/api';
+
+export const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dzc7q0xpc/image/upload';
+
+export const DEFAULT_IMAGE = 'https://res.cloudinary.com/dzc7q0xpc/image/upload/v1749415547/default-recipe.jpg'; 
